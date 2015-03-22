42bcb38f7c6aefb35209bdfcf2b0452600c5394b
Tris Forster <tris@tfconsulting.com.au>
2015-03-23 10:25:26 +1100

    Added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    version='0.1',
+    description='RepriseDB',
+    author='Tris Forster',
+    url='https://github.com/tf198/reprisedb',
+    packages=['reprisedb']
+)
